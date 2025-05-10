62d13472869907c07ae471b25b1cef60d710bca2
Alex Stanciu <ealexs@gmail.com>
2025-05-10 13:18:40 +0300

    update

--- install.sh
+++ install.sh
@@ -14,5 +14,11 @@ ls -al $INSTALL_DIR;
 
 git clone https://github.com/alexstanciu-1/dp-vm "$INSTALL_DIR/vm"
 
+mkdir "$INSTALL_DIR/lib"
+
+git clone https://github.com/alexstanciu-1/omi-frame "$INSTALL_DIR/lib/omi-frame"
+git clone https://github.com/alexstanciu-1/descriptivejs "$INSTALL_DIR/lib/descriptivejs"
+git clone https://github.com/alexstanciu-1/dp-engine.git "$INSTALL_DIR/lib/dp-engine"
+
 cd "$INSTALL_DIR/vm/vagrant" && vagrant provision
 cd "$INSTALL_DIR/vm/vagrant" && vagrant up
